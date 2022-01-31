76c1cb531264b6040c4ee304c3895f5613001c84
owiniose <98717429+owiniose@users.noreply.github.com>
2022-01-31 19:58:11 +0100

    Delete index.js

--- hardhat-tutorial/constants/index.js
+++
@@ -1,6 +0,0 @@
-// Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x160Ed384D4E9BB2348461cCB2FAAe3daa7498B70";
-// URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
-
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
