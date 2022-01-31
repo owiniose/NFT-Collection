ef050a47826b583a281ed1985de2003cddfdaffe
owiniose <98717429+owiniose@users.noreply.github.com>
2022-01-31 19:58:56 +0100

    Add files via upload

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xd5dBb3960447c0495E996FC135BE0e6ef9Ac98f6";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-omega.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
